89fb526abefb21f1369d49b214991524c27b7741
Laura Lopez <laura.lopez1@etu.unice.fr>
2019-10-13 17:40:20 +0200

    add notif consumer and consumer config utils

---
+++ src/main/java/fr/unice/polytech/si5/al/creditrama/teamd/notificationservice/utils/ConsumerConfig.java
@@ -0,0 +1,45 @@
+package fr.unice.polytech.si5.al.creditrama.teamd.notificationservice.utils;
+
+import fr.unice.polytech.si5.al.creditrama.teamd.notificationservice.service.SendNotifConsumer;
+import fr.unice.polytech.si5.al.creditrama.teamd.notificationservice.service.SendNotifProducer;
+
+import java.util.Properties;
+
+public class ConsumerConfig {
+
+    public static SendNotifConsumer configConsumer(String kafkaBrokers) {
+        Properties props = new Properties();
+        props.setProperty("group.id", SendNotifConsumer.class.getCanonicalName());
+        props.setProperty("enable.auto.commit", "true");
+        props.setProperty("bootstrap.servers", kafkaBrokers);
+        props.setProperty("auto.commit.interval.ms", "100");
+        props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        return new SendNotifConsumer(props);
+    }
+
+    public static SendNotifConsumer configConsumer(String groupId, String kafkaBrokers) {
+        Properties props = new Properties();
+        props.setProperty("group.id", groupId);
+        props.setProperty("bootstrap.servers", kafkaBrokers);
+        props.setProperty("enable.auto.commit", "true");
+        props.setProperty("auto.commit.interval.ms", "100");
+        props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        return new SendNotifConsumer(props);
+    }
+
+    public static SendNotifProducer configProducer(String groupId, String kafkaBrokers) {
+        Properties props = new Properties();
+        props.setProperty("group.id", groupId);
+        props.setProperty("bootstrap.servers", kafkaBrokers);
+        props.setProperty("enable.auto.commit", "true");
+        props.setProperty("auto.commit.interval.ms", "100");
+        props.setProperty("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.setProperty("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        return new SendNotifProducer(props);
+    }
+}
