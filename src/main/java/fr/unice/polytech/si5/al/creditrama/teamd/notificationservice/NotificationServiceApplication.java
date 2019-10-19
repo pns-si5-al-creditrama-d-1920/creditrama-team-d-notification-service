7497a0834005c2bef241b0448279584b1284132e
Laura Lopez <laura.lopez1@etu.unice.fr>
2019-10-19 19:31:49 +0200

    Add kafka consumer, now an email is sent on bank account creation

--- src/main/java/fr/unice/polytech/si5/al/creditrama/teamd/notificationservice/NotificationServiceApplication.java
+++ src/main/java/fr/unice/polytech/si5/al/creditrama/teamd/notificationservice/NotificationServiceApplication.java
@@ -1,25 +1,67 @@
 package fr.unice.polytech.si5.al.creditrama.teamd.notificationservice;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import fr.unice.polytech.si5.al.creditrama.teamd.notificationservice.controller.NotificationsController;
 import fr.unice.polytech.si5.al.creditrama.teamd.notificationservice.service.NotificationProcessor;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Payload;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
 
 @EnableBinding(NotificationProcessor.class)
 @SpringBootApplication
 public class NotificationServiceApplication {
 
     @StreamListener("CreditRama.SendNotif")
-    public void sendNotif(@Payload String val) {
-        System.out.println("NOTIF OK " + val);
+    public void sendNotif(@Payload String val) throws IOException {
+        /*System.out.println("NOTIF OK " + val);
+        JsonObject jsonObject = new JsonObject().getAsJsonObject(val);
+        JsonElement emailClient = jsonObject.get("email");
+        if (emailClient != null) {
+            NotificationsController notificationsController = new NotificationsController();
+            System.out.println("ici parce que le mail est reconnu");
+            notificationsController.testMail(emailClient.toString(), "Bonjour,\n Nous vous remercions d'avoir créé votre compte chez CreditRama.\n " +
+                    "Ceci est un mail de confirmation, merci de ne pas répondre.\n \n Cordialement,\n L'équipe CreditRama");
+        }*/
     }
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
-        /*SendNotifConsumer sendNotifConsumer = ConsumerConfig.configConsumer("CreditRama.NotificationService", "127.0.0.1:9092");
-        sendNotifConsumer.consume("CreditRama.SendNotif");*/
-    }
+        Properties props = new Properties();
+        props.setProperty("group.id", "Laura.Test");
+        props.setProperty("enable.auto.commit", "true");
+        props.setProperty("bootstrap.servers", "localhost:9092");
+        props.setProperty("auto.commit.interval.ms", "100");
+        props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
+        try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props)) {
+            consumer.subscribe(Collections.singletonList("CreditRama.SendNotif"));
+
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<String, String> record : records) {
+                    JSONObject jsonObj = new JSONObject(record.value().toString());
+                    if (jsonObj.get("email") != null) {
+                        NotificationsController notificationsController = new NotificationsController();
+                        notificationsController.testMail(jsonObj.get("email").toString(), "Bonjour,\n Nous vous remercions d'avoir créé votre compte chez CreditRama.\n " +
+                                "Ceci est un mail de confirmation, merci de ne pas répondre.\n \n Cordialement,\n L'équipe CreditRama");
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/fr/unice/polytech/si5/al/creditrama/teamd/notificationservice/controller/NotificationsController.java
+++ src/main/java/fr/unice/polytech/si5/al/creditrama/teamd/notificationservice/controller/NotificationsController.java
@@ -7,13 +7,14 @@ import com.sendgrid.SendGrid;
 import com.sendgrid.helpers.mail.Mail;
 import com.sendgrid.helpers.mail.objects.Content;
 import com.sendgrid.helpers.mail.objects.Email;
+import com.sendgrid.helpers.mail.objects.Personalization;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.stream.Stream;
 
 @RestController
 public class NotificationsController {
@@ -22,16 +23,17 @@ public class NotificationsController {
 
     @CrossOrigin(origins = "*")
     @GetMapping("/testMail")
-    public void testMail(String message) throws IOException {
+    public void testMail(String emails, String message) throws IOException {
         Email from = new Email("noreply@creditrama.com");
-        String subject = "Creditrama la meilleur banque en ligne";
-        //Content content = new Content("text/plain", "J'envoie ce mail suite à un évènement consommé dans Kafka, et ça, c'est vraiment fort.");
+
+        String subject = "Creditrama, la meilleur banque en ligne !";
         Content content = new Content("text/plain", message);
 
         SendGrid sg = new SendGrid(sendgridkey);
-        Arrays.asList("alexisgardin@gmail.com", "lauraalop7@gmail.com", "nathan.strobbe@gmail.com").stream().forEach(email -> {
+        Stream.of(emails).forEach(email -> {
             Email to = new Email(email);
             Mail mail = new Mail(from, subject, to, content);
+            //mail.setTemplateId("d-e05e286316694c3a92fd9a8c6e6112e8");
 
             Request request = new Request();
             request.setMethod(Method.POST);
@@ -43,11 +45,10 @@ public class NotificationsController {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            assert response != null;
             System.out.println(response.getStatusCode());
             System.out.println(response.getBody());
             System.out.println(response.getHeaders());
         });
-
-
     }
 }
