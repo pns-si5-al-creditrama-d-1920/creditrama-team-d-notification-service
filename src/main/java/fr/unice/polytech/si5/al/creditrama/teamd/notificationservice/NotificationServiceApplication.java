ce786d09dee7af1cd1bc6c836bc93f29143c792f
Alexis Gardin <alexis.gardin@univ-cotedazur.fr>
2020-01-17 21:55:54 +0100

    Add email notification

--- src/main/java/fr/unice/polytech/si5/al/creditrama/teamd/notificationservice/NotificationServiceApplication.java
+++ src/main/java/fr/unice/polytech/si5/al/creditrama/teamd/notificationservice/NotificationServiceApplication.java
@@ -1,28 +1,16 @@
 package fr.unice.polytech.si5.al.creditrama.teamd.notificationservice;
 
-import fr.unice.polytech.si5.al.creditrama.teamd.notificationservice.controller.NotificationsController;
-import fr.unice.polytech.si5.al.creditrama.teamd.notificationservice.models.Notification;
-import fr.unice.polytech.si5.al.creditrama.teamd.notificationservice.service.NotificationProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.context.annotation.Profile;
-import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableBinding(NotificationProcessor.class)
 @SpringBootApplication
-@Profile("!disable-kafka")
+@EnableFeignClients
 public class NotificationServiceApplication {
-
-    @StreamListener("CreditRama.SendNotif")
-    public void sendNotif(@Payload Notification val) throws Exception {
-        if (val.getType().equals("EMAIL")) {
-            new NotificationsController().sendMail(val);
-        }
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
     }
+
+
 }
---
+++ src/main/java/fr/unice/polytech/si5/al/creditrama/teamd/notificationservice/models/TransactionDTO.java
@@ -0,0 +1,25 @@
+package fr.unice.polytech.si5.al.creditrama.teamd.notificationservice.models;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class TransactionDTO {
+    private String uuid;
+
+    private BankAccount source;
+
+    private BankAccount dest;
+
+    private double amount;
+
+    private LocalDateTime createdTransaction;
+
+
+}
